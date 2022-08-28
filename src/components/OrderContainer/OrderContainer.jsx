11f65555cde4fa848c3dd7e96754353222ecfcf4
Safat Kamal <kamalashar54@gmail.com>
2022-08-28 16:26:17 +0530

    Created Checkout page with order details

--- src/components/OrderContainer/OrderContainer.jsx
+++ src/components/OrderContainer/OrderContainer.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useCartContextValue } from '../../context/CartProvider';
-import CartItemRow from '../CartItemRow/CartItemRow';
 import './OrderContainer.css';
 import Grid from '@mui/material/Grid';
 import { Link } from 'react-router-dom';
 import { getcartTotalPrice } from '../../context/reducer';
+import OrderItemRow from '../OrderItemRow/OrderItemRow';
 
 const OrderContainer = () => {
     const [{ cart }] = useCartContextValue();
@@ -15,20 +15,27 @@ const OrderContainer = () => {
                     <Grid item xs={12} sm={12} md={12} lg={8} xl={8}>
                         <div className="OrderContainer__left">
                             <h3 id="Order_title">Order Details</h3>
-                            {cart.length ? cart.map((item) => {
-                                return (<CartItemRow key={item.id} id={item.id} dish={item.dish} image={item.image} quantity={item.quantity} price={item.price} addons={item.addons}/>);
-                            }) : <Link to="/">
-                                    <button type="button" id="continue_explore">Explore Food</button>
-                                </Link>
-                            }
+                            {cart.map((item) => {
+                                return (<OrderItemRow key={item.id} id={item.id} dish={item.dish} image={item.image} quantity={item.quantity} price={item.price} addonsTotal={item.AddonTotalPrice}/>);
+                            })}
                         </div>
                     </Grid>
                     <Grid item xs={12} sm={12} md={12} lg={4} xl={4} >
-                        <div className="cartContainer__right">
-                            <h3>SubTotal:  <span>₹{getcartTotalPrice(cart)}</span></h3>
-                            <Link to="/checkout">
-                                <button type="button" id="proceed_checkout" disabled={(getcartTotalPrice(cart)> 0)? false: true}>Buy Now</button>
-                            </Link>
+                        <div className="orderContainer__right">
+                            <h3>Order Summary</h3>
+                            <div id="order_right_itemTotal">
+                                <p>Item Total: </p>
+                                <p>₹{getcartTotalPrice(cart)}</p>
+                            </div>
+                            <div id="order_right_DeliveryTotal">
+                                <p>Delivery Charge: </p>
+                                <p>₹20</p>
+                            </div>
+                            <div id="order_right_Total">
+                                <p>Grand Total: </p>
+                                <p>₹{getcartTotalPrice(cart) + 20}</p>
+                            </div>
+                            <button type="button" id="buyNow" disabled={(getcartTotalPrice(cart)> 0)? false: true}>Buy Now</button>
                         </div>
                     </Grid>
                 </Grid>
--- src/components/OrderItemRow/OrderItemRow.jsx
+++ src/components/OrderItemRow/OrderItemRow.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import './OrderItemRow.css';
+import { useCartContextValue } from '../../context/CartProvider';
 
-const OrderItemRow = () => {
-  return (
-    <div>OrderItemRow</div>
-  )
+
+const OrderItemRow = ({ id, dish, image, quantity, price, addonsTotal }) => {
+    const [, dispatch] = useCartContextValue();
+
+    return (
+        <>
+            <div className='item__row'>
+                <span className="order_quantity">{quantity} x </span><img src={image} className="item_image" alt={dish} />
+                <div className='item_description'>
+                    <div className="item_description_price">
+                        <p className='item_name'>{dish}</p>
+                        <p className='item_price'>Price: ₹{price}</p>
+                    </div>
+                    <p className="item_addonTotal_price">
+                        {addonsTotal > 0 && `Addons: ₹${addonsTotal}`}
+                    </p>
+                </div>
+            </div>
+            <p className="item_Total">
+                Total: ₹{addonsTotal + (quantity*price)}
+            </p>
+        </>
+    )
 }
 
 export default OrderItemRow
